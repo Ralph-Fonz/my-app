d1e68294af5f90a11237f47fee31c5c51b9cc591
geraltofrivia69 <charneuk@gmail.com>
2021-06-14 20:48:50 -0500

    Background and image
    added background and image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import './App.css';
+import React from "react";
+import "./styles.css";
+
+
 
 function App () {
-  return <h1> Hello </h1>;
+  return ( 
+
+<div>
+  <body>
+  <img src ="/images/D2R.jpg" alt = "Diablo 2 Resurrected" />
+  
+
+  <div class= "w3-container w3-blue w3-border">
+    Hi
+  </div>
+  </body>
+</div>
+
+  );
 }
 
 
